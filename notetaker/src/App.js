9c72a560133402f49dc434c8b1121d01df27a27d
HotsumaX <luislaborde@gmail.com>
2019-08-14 22:05:28 -0400

    added the functions to handle the change note and add note with the graphql operation

--- notetaker/src/App.js
+++ notetaker/src/App.js
@@ -1,14 +1,30 @@
 import React, { useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
+import { createNote } from './graphql/mutations';
 
 const App = () => {
   const [note, setNote] = useState('');
   const [notes, setNotes] = useState([]);
+
+  const handleChangeNote = event => setNote(event.target.value);
+
+  const handleAddNote = event => {
+    event.preventDefault();
+    const input = { note };
+    API.graphql(graphqlOperation(createNote, { input }));
+  };
+
   return (
     <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
       <h1 className="code f2-1">Amplify Notetaker</h1>
-      <form action="" className="mb3">
-        <input type="text" className="pa2 f4" placeholder="Write your note" />
+      <form action="" className="mb3" onSubmit={handleAddNote}>
+        <input
+          type="text"
+          className="pa2 f4"
+          placeholder="Write your note"
+          onChange={handleChangeNote}
+        />
         <button className="pa2 f4">Add Note</button>
       </form>
 
