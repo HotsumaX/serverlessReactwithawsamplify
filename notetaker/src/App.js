348f60c19b3a1f99ab33b294a61d5dd1a92ba3ca
HotsumaX <luislaborde@gmail.com>
2019-08-14 20:31:43 -0400

    fixed the node moduales and set up the header, form for taking a note and placeholder for the list of notes

--- notetaker/src/App.js
+++ notetaker/src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withAuthenticator } from 'aws-amplify-react';
 
-function App() {
-  return <div>app</div>;
-}
+const App = () => {
+  const [notes, setNotes] = useState([{ id: 1, note: 'Hello world' }]);
+  return (
+    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+      <h1 className="code f2-1">Amplify Notetaker</h1>
+      <form action="" className="mb3">
+        <input type="text" className="pa2 f4" placeholder="Write your note" />
+        <button className="pa2 f4">Add Note</button>
+      </form>
+
+      <div>
+        {notes.map(item => (
+          <div key={item.id} className="flex items-center">
+            <li className="list pa1 f3">{item.note}</li>
+            <button className="bg-transparent bn f4">
+              <span>&times;</span>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default withAuthenticator(App, { includeGreetings: true });
