83fdf74503693df08b0659ff2222d6349f96f53d
HotsumaX <luislaborde@gmail.com>
2019-08-15 11:53:08 -0400

    added the delete note function and the useEffect function to bring up the list of notes

--- notetaker/src/App.js
+++ notetaker/src/App.js
@@ -1,18 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
-import { createNote } from './graphql/mutations';
+import { createNote, deleteNote } from './graphql/mutations';
+import { listNotes } from './graphql/queries';
 
 const App = () => {
   const [note, setNote] = useState('');
   const [notes, setNotes] = useState([]);
 
+  useEffect(() => {
+    async function fetchData() {
+      const result = await API.graphql(graphqlOperation(listNotes));
+      setNotes([...result.data.listNotes.items]);
+    }
+    fetchData();
+  }, []);
+
   const handleChangeNote = event => setNote(event.target.value);
 
-  const handleAddNote = event => {
+  const handleAddNote = async event => {
     event.preventDefault();
     const input = { note };
-    API.graphql(graphqlOperation(createNote, { input }));
+    const result = await API.graphql(graphqlOperation(createNote, { input }));
+    const newNote = result.data.createNote;
+    setNotes([newNote, ...notes]);
+    setNote('');
+  };
+
+  const handleDeleteNote = async id => {
+    const input = { id };
+    const result = await API.graphql(graphqlOperation(deleteNote, { input }));
+    const deletedNoteId = result.data.deleteNote.id;
+    const updatedNotes = notes.filter(note => note.id !== deletedNoteId);
+    setNotes([...updatedNotes]);
   };
 
   return (
@@ -24,6 +44,7 @@ const App = () => {
           className="pa2 f4"
           placeholder="Write your note"
           onChange={handleChangeNote}
+          value={note}
         />
         <button className="pa2 f4">Add Note</button>
       </form>
@@ -32,7 +53,10 @@ const App = () => {
         {notes.map(item => (
           <div key={item.id} className="flex items-center">
             <li className="list pa1 f3">{item.note}</li>
-            <button className="bg-transparent bn f4">
+            <button
+              className="bg-transparent bn f4"
+              onClick={() => handleDeleteNote(item.id)}
+            >
               <span>&times;</span>
             </button>
           </div>
