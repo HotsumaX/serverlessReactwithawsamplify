b61cfae498c03d1fe4b077b11f9124affa0898a2
HotsumaX <luislaborde@gmail.com>
2019-08-20 17:06:49 -0400

    added back agora

---
+++ amplifyagora/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  state = {};
+
+  render() {
+    return <div>App test</div>;
+  }
+}
+
+export default App;
