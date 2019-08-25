7a405ed1f9ca75393aff9dd61a72edb21c3946f8
HotsumaX <luislaborde@gmail.com>
2019-08-25 08:49:37 -0400

    first attempt at disconnecting the aws server from the product, it added an elasticsearch service. i have speculations that the app may have a bug causing the application to query the server over time

--- amplifyagora/src/App.js
+++ amplifyagora/src/App.js
@@ -5,7 +5,12 @@ class App extends React.Component {
   state = {};
 
   render() {
-    return <div>App test to update the aws services, testing keys</div>;
+    return (
+      <div>
+        App test to update the aws services, testing keys, removed the
+        connection to aws
+      </div>
+    );
   }
 }
 
