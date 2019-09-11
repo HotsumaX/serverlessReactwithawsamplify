8289cca1b6304d0ec7526fb86f5e21e0d160f8f5
HotsumaX <luislaborde@gmail.com>
2019-09-11 11:06:20 -0400

    added the list of markets

--- amplifyagora/src/components/MarketList.js
+++ amplifyagora/src/components/MarketList.js
@@ -1,8 +1,84 @@
 import React from 'react';
-// import { Loading, Card, Icon, Tag } from "element-react";
+import { graphqlOperation } from 'aws-amplify';
+import { Connect } from 'aws-amplify-react';
+import { listMarkets } from '../graphql/queries';
+import { onCreateMarket } from '../graphql/subscriptions';
+import Error from './Error';
+import { Loading, Card, Icon, Tag } from 'element-react';
+import { Link } from 'react-router-dom';
 
 const MarketList = () => {
-  return <div>MarketList</div>;
+  const onNewMarket = (prevQuery, newData) => {
+    let updatedQuery = { ...prevQuery };
+    const updatedMarketList = [
+      newData.onCreateMarket,
+      ...prevQuery.listMarkets.items,
+    ];
+    updatedQuery.listMarkets.items = updatedMarketList;
+    return updatedQuery;
+  };
+  return (
+    <Connect
+      query={graphqlOperation(listMarkets)}
+      subscription={graphqlOperation(onCreateMarket)}
+      onSubscriptionMsg={onNewMarket}
+    >
+      {({ data, loading, errors }) => {
+        if (errors.length > 0) return <Error errors={errors} />;
+        if (loading || !data.listMarkets) return <Loading fullscreen={true} />;
+        return (
+          <>
+            <h2 className="header">
+              <img
+                src="https://icon.now.sh/store_mall_directory/527FF"
+                alt="Store Icon"
+                className="large-icon"
+              />
+              Markets
+            </h2>
+            {data.listMarkets.items.map(market => (
+              <div className="my-2" key={market.id}>
+                <Card
+                  bodyStyle={{
+                    padding: '0.7em',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <div>
+                    <span className="flex">
+                      <Link className="link" to={`/markets/${market.id}`}>
+                        {market.name}
+                      </Link>
+                      <span style={{ color: 'var(--darkAmazonOrange)' }}>
+                        {/* {market.products.items.length} */}
+                      </span>
+                      <img
+                        src="https://icon.now.sh/shopping_cart/f60"
+                        alt="shopping"
+                      />
+                    </span>
+                    <div style={{ color: 'var(--lightSquidInk)' }}>
+                      {market.owner}
+                    </div>
+                  </div>
+                  <div>
+                    {market.tags &&
+                      market.tags.map(tag => (
+                        <Tag className="mx-1" type="danger" key={tag}>
+                          {tag}
+                        </Tag>
+                      ))}
+                  </div>
+                </Card>
+              </div>
+            ))}
+          </>
+        );
+      }}
+    </Connect>
+  );
 };
 
 export default MarketList;
