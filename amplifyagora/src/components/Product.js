644200e9a8d632ef4823aed503ae839921c8466b
HotsumaX <luislaborde@gmail.com>
2019-09-23 15:53:21 -0400

    added the pay button in each product

--- amplifyagora/src/components/Product.js
+++ amplifyagora/src/components/Product.js
@@ -3,6 +3,8 @@ import { S3Image } from 'aws-amplify-react';
 import { convertCentsToDollars } from '../utils/index';
 // prettier-ignore
 import { Notification, Popover, Button, Dialog, Card, Form, Input, Radio } from "element-react";
+import { UserContext } from '../App';
+import PayButton from './PayButton';
 
 class Product extends React.Component {
   state = {};
@@ -11,30 +13,40 @@ class Product extends React.Component {
     const { product } = this.props;
 
     return (
-      <div className="card-container">
-        <Card bodyStyle={{ padding: 0, minWidth: '200px' }}>
-          <S3Image
-            imgKey={product.file.key}
-            theme={{ photoImg: { maxWidth: '100%', maxHeight: '100%' } }}
-          />
-          <div className="card-body">
-            <h3 className="m-0">{product.description}</h3>
-            <div className="items-center">
-              <img
-                src={`https://icon.now.sh/${
-                  product.shipped ? 'markunread_mailbox' : 'mail'
-                }`}
-                alt="Shipping Icon"
-                className="icon"
-              />
-              {product.shipped ? 'Shipped' : 'emailed'}
+      <UserContext.Consumer>
+        {({ user }) => {
+          const isProductOwner = user && user.attributes.sub === product.owner;
+          return (
+            <div className="card-container">
+              <Card bodyStyle={{ padding: 0, minWidth: '200px' }}>
+                <S3Image
+                  imgKey={product.file.key}
+                  theme={{ photoImg: { maxWidth: '100%', maxHeight: '100%' } }}
+                />
+                <div className="card-body">
+                  <h3 className="m-0">{product.description}</h3>
+                  <div className="items-center">
+                    <img
+                      src={`https://icon.now.sh/${
+                        product.shipped ? 'markunread_mailbox' : 'mail'
+                      }`}
+                      alt="Shipping Icon"
+                      className="icon"
+                    />
+                    {product.shipped ? 'Shipped' : 'emailed'}
+                  </div>
+                  <div className="text-right">
+                    <span className="mx-1">
+                      ${convertCentsToDollars(product.price)}
+                    </span>
+                    {!isProductOwner && <PayButton />}
+                  </div>
+                </div>
+              </Card>
             </div>
-            <div className="text-right">
-              <span className="mx-1" />
-            </div>
-          </div>
-        </Card>
-      </div>
+          );
+        }}
+      </UserContext.Consumer>
     );
   }
 }
